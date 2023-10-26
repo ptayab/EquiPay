34af1a26350afe3624030a1276969102dcdfdd29
Yuzhe Ruan <yur764@usask.ca>
2023-10-25 19:20:46 -0600

    add forder and move file let easy to control

--- equi-pay-app/src/components/UserList.jsx
+++ equi-pay-app/src/components/UserList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './userList.css'
+import '../styles/userList.css'
 
 function UserList() {
   const sampleUsers = [
--- equi-pay-app/src/index.js
+++ equi-pay-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
