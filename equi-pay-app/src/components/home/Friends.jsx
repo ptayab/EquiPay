9a374e07d35e9cbfb3873f3588bc7a443497845c
Hao Wu <roylunnwall@gmail.com>
2023-10-29 16:26:56 +0800

    Feature: Main page ui
    1. Create and join group
    2. Add friend
    3. Create expense dialog

---
+++ equi-pay-app/src/components/home/Friends.jsx
@@ -0,0 +1,40 @@
+import GroupsIcon from "@mui/icons-material/Groups";
+import {Button, IconButton} from "@mui/material";
+import React from "react";
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import AddIcon from "@mui/icons-material/Add";
+import AddFriend from "./AddFriend";
+
+function Friends() {
+    const friends = [
+        { name: 'Friend 1', needPay: 12 },
+        { name: 'Friend 2', needPay: 8 },
+        { name: 'Friend 3', needPay: 17 },
+    ];
+
+    return (
+        <div className="container mx-auto mt-5">
+            <div className={'flex items-center gap-2'}>
+                <Button color={'error'} startIcon={<PeopleOutlineIcon />}>
+                    Friend
+                </Button>
+                <AddFriend />
+            </div>
+            <ul className="space-y-4">
+                {friends.map((friend, index) => (
+                    <li key={index} className="flex items-center p-4 rounded-lg shadow-md bg-white">
+                        <div className="rounded-full h-8 w-8 bg-blue-500 text-white flex items-center justify-center mr-4">
+                            {friend.needPay}
+                        </div>
+                        <div>
+                            <h2 className="text-lg font-semibold">{friend.name}</h2>
+                            <p className="text-gray-600">Mutual Friends: {friend.needPay}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Friends;
--- equi-pay-app/src/components/login.jsx
+++ equi-pay-app/src/components/login.jsx
@@ -1,74 +1,87 @@
-import React, { Component } from 'react';
-import {useState} from 'react';
-import Axios from 'axios';
-import { Link, Navigate } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-//import { Home } from './Home';
-//import './Login.css'
+import React from 'react';
+import { Box, Container, Paper, Typography, TextField, Button, Link as MUILink } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import {Link, useNavigate} from 'react-router-dom';
 
-export const Login = ({set}) => {
-const [user, setUser] = useState(null);
-
-const [username, setUsername] = useState('');
-const [password, setPassword]= useState('');
-
-const [LoginStatus, setLoginStatus]= useState('');
-const [loggedIn, setLoggedIn] = useState('');
-
-const nav = useNavigate();
-
-const login = () => {
-Axios.post("http://localhost:8080/login",{
-username: username,
-password: password,
-}).then((response) => {   
-
-if(response.status == 200 && response.data.user){
-console.log(response.data.user);
-setLoggedIn("true");
-setUser(response.data.user);
-//nav('/home', { state: { user: response.data.user } });
-setLoginStatus("success")
-
-}
-else{
-
-console.log(response)
-setLoggedIn("false");
-console.log("Wrong username / password")
-setLoginStatus("Wrong username / password! Please try again")
-}
-
+const validationSchema = yup.object({
+    email: yup.string().email('Invalid email').required('Email is required'),
+    password: yup.string().required('Password is required'),
 });
-};
 
-return(
-    <div className="App">
+function LoginPage() {
+    const navigate = useNavigate();
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            navigate('/')
+        },
+    });
 
-<div className='Log-In'>
-                <h1>Log In</h1>
-                <label>Username</label>
-                <input
-                    required type = "text"
-                    onChange= { (e) => {
-                        setUsername(e.target.value);
-                    }
-                    }
-                    /><br/>
-                <label>password</label>
-                <input
-                    required type = "password"
-                    onChange= { (e) => {
-                        setPassword(e.target.value);
-                    }
-                    }
-                    /><br/>
-                <button onClick={login}>Log In</button><br/>
-                <span>Create account? <Link to="/register">Register</Link></span>
-            </div>
-            <p>{ LoginStatus }</p>
-    </div>
-)
-};
+    const handleFieldBlur = (field) => {
+        formik.setFieldTouched(field, true);
+        if (formik.values[field] === '') {
+            formik.setFieldError(field, 'This field is required');
+        }
+    };
 
-export default Login;
+    return (
+        <Container maxWidth="sm">
+            <Paper style={{marginTop: '100px'}} elevation={3}>
+                <Box p={4}>
+                    <Typography variant="h4" align="center" gutterBottom>
+                        Login
+                    </Typography>
+                    <form onSubmit={formik.handleSubmit}>
+                        <TextField
+                            fullWidth
+                            id="email"
+                            name="email"
+                            label="Email"
+                            variant="outlined"
+                            margin="normal"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={() => handleFieldBlur('email')}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email}
+                        />
+                        <TextField
+                            fullWidth
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            margin="normal"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={() => handleFieldBlur('password')}
+                            error={formik.touched.password && Boolean(formik.errors.password)}
+                            helperText={formik.touched.password && formik.errors.password}
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            size="large"
+                            style={{ marginTop: '20px' }}
+                        >
+                            Login
+                        </Button>
+                    </form>
+                    <Typography variant="body1" align="center" style={{ marginTop: '20px' }}>
+                        Don't have an account? <MUILink component={Link} to="/register">Register</MUILink>
+                    </Typography>
+                </Box>
+            </Paper>
+        </Container>
+    );
+}
+
+export default LoginPage;
