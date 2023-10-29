0bb533ac66599fab5d2f518ed9153c9981f20e53
Hao Wu <roylunnwall@gmail.com>
2023-10-29 16:26:56 +0800

    Feature: Main page ui
    1. Create and join group
    2. Add friend
    3. Create expense dialog

---
+++ equi-pay-app/src/components/common/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem, Link } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenuClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={handleMenuClick}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+                    <MenuItem onClick={handleMenuClose}>About</MenuItem>
+                    <MenuItem onClick={handleMenuClose}>Services</MenuItem>
+                    <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+                </Menu>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                    Your Website
+                </Typography>
+                <Link href="#" color="inherit">Home</Link>
+                <Link href="#" color="inherit">About</Link>
+                <Link href="#" color="inherit">Services</Link>
+                <Link href="#" color="inherit">Contact</Link>
+                <Button color="inherit">Login</Button>
+                <Button color="inherit">Register</Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
