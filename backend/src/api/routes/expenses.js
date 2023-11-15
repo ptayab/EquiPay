dbb56a5d3d57ff5d5cbd9c4868909d064113d92b
Steven <yur764@usask.ca>
2023-11-15 11:53:16 -0600

    fix the expense for other people to look

--- backend/src/api/routes/expenses.js
+++ backend/src/api/routes/expenses.js
@@ -24,8 +24,6 @@ export default (app) => {
             res.json(await Database.getEntries(
                 'expenses', 
                 { 
-                    id: id ? id : "*",
-                    user_id: user_id ? user_id : "*",
                     group_id: group_id ? group_id : "*",   
                 }
             ));
